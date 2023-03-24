28d9127df2773c2da10497eeab41643f0e9f83da
Mohammod Ali <mohammodali651@gmail.com>
2023-03-25 01:40:48 +0600

    carousel added

--- src/Pages/Carousel/Carousel.js
+++ src/Pages/Carousel/Carousel.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React from "react";
 
 const Carousel = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <h1>Carousel Effect</h1>
+      <div class="MagicScroll" data-options="mode: carousel; height: 275px;">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuG7SNaps-wHLv3fKdSS7zd9VRPB_JSVrKYQ&usqp=CAU" />
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-6Ipo3QVEQ9mJbPd3FOoNSMu5lYAnZwcu1A&usqp=CAU" />
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRL5BC6cMrBxjyDFjQhtFAqH0u-CzFSHxADxg&usqp=CAU" />
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRL5BC6cMrBxjyDFjQhtFAqH0u-CzFSHxADxg&usqp=CAU" />
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTH6hXdrn3wlFdLErTzO18EnoBRycmpQKl10w&usqp=CAU" />
+      </div>
+      <h1>Cover Flow Effect</h1>
+      <div class="MagicScroll" data-options="mode: cover-flow; height: 275px;">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuG7SNaps-wHLv3fKdSS7zd9VRPB_JSVrKYQ&usqp=CAU" />
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-6Ipo3QVEQ9mJbPd3FOoNSMu5lYAnZwcu1A&usqp=CAU" />
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRL5BC6cMrBxjyDFjQhtFAqH0u-CzFSHxADxg&usqp=CAU" />
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRL5BC6cMrBxjyDFjQhtFAqH0u-CzFSHxADxg&usqp=CAU" />
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTH6hXdrn3wlFdLErTzO18EnoBRycmpQKl10w&usqp=CAU" />
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuG7SNaps-wHLv3fKdSS7zd9VRPB_JSVrKYQ&usqp=CAU" />
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-6Ipo3QVEQ9mJbPd3FOoNSMu5lYAnZwcu1A&usqp=CAU" />
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRL5BC6cMrBxjyDFjQhtFAqH0u-CzFSHxADxg&usqp=CAU" />
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRL5BC6cMrBxjyDFjQhtFAqH0u-CzFSHxADxg&usqp=CAU" />
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTH6hXdrn3wlFdLErTzO18EnoBRycmpQKl10w&usqp=CAU" />
+      </div>
+    </div>
+  );
 };
 
 export default Carousel;
